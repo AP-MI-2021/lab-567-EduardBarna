305e9302f6ad39a303c6b2417f57e79217cf2d43
EduardBarna <eduard.barna@stud.ubbcluj.ro>
2021-11-03 03:10:18 +0200

    2 implementari Domain

---
+++ Logic/CRUD.py
@@ -0,0 +1,58 @@
+from Domain.vanzare import creaza_vanzare, getId
+
+
+def getById(id, lista):
+    '''
+    Returneaza o vanzare cu id-ul dat
+    :param id:id-ul cautat
+    :return:vanzarea cu id-ul cautat
+    '''
+    for v in lista:
+        if getId(v) == id:
+            return v
+    return None
+
+def adauga_vanzare(id, titlu, gen, pret, tip_reducere ,lista):
+    '''
+    Adauga o vanzare la o lista ce contine alte vanzari
+    :param id: id-ul cartii
+    :param titlu: titlul cartii
+    :param gen: genul cartii
+    :param pret: pretul cartii
+    :param tip_reducere: tipul reducerii clientului
+    :param lista: lista initiala de vanzari
+    :return: lista initiala + vanzarea adaugata
+    '''
+    vanzare = creaza_vanzare(id, titlu, gen, pret, tip_reducere)
+    return lista +[vanzare]
+
+def sterge_vanzare(id, lista):
+    '''
+    Sterge o vanzare cu id-ul dat dintr-o lista
+    :param id: id-ul dat
+    :param lista: lista initiala
+    :return: lista fara elementul cu id-ul dat
+    '''
+    return [v for v in lista if getId(v) != id]
+
+def modifica_vanzare(id, titlu, gen, pret, tip_reducere,lista):
+    '''
+    Modifica o vanzare cu id-ul dat
+    :param id: id-ul cautat
+    :param titlu: Noul titlu
+    :param gen: noul gen
+    :param pret: noul pret
+    :param tip_reducere: noul tip de reducere
+    :param lista: lista initiala
+    :return: noua lista
+    '''
+    listaNoua = []
+    for v in lista:
+        if getId(v) == id:
+            vanzare_noua = creaza_vanzare(id, titlu, gen, pret, tip_reducere)
+            listaNoua.append(vanzare_noua)
+        else:
+            listaNoua.append(v)
+    return listaNoua
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from Teste.testALL import test_all
+from UI.Consola import runMenu
+
+
+def main():
+    test_all()
+    runMenu()
+
+if __name__ == "__main__":
+    main()
+
